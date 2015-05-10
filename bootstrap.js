9864b6866e96167afecbb997cdcd44d7c0a1ba03
Alexander Davydov <nyddle@gmail.com>
2015-05-11 00:38:33 +0300

    it works

--- bootstrap.js
+++ bootstrap.js
@@ -1,22 +1,31 @@
 // if the database is empty on server start, create some sample data.
 Meteor.startup(function () {
-  if (Events.find().count() === 0) {
-    var data = [
-      {name: "Meteor Principles",
-       text: "Some event..."
-      },
-      {name: "Languages",
-       text: "Some event..."
-      },
-      {name: "Favorite Scientists",
-       text: "Some event..."
+
+  if (Meteor.isServer) {
+
+      Events.remove({});
+
+      if (Events.find().count() === 0) {
+        var data = [
+          {name: "Meteor Principles",
+           app: "app1",
+           text: "Some event..."
+          },
+          {name: "Languages",
+           app: "app1",
+           text: "Some event..."
+          },
+          {name: "Favorite Scientists",
+           app: "app2",
+           text: "Some event..."
+          }
+        ];
+
+        var timestamp = (new Date()).getTime();
+        _.each(data, function(list) {
+          var event_id = Events.insert({name: list.name, text: list.text, app: list.app, createdAt: new Date(timestamp) });
+
+        });
       }
-    ];
-
-    var timestamp = (new Date()).getTime();
-    _.each(data, function(list) {
-      var event_id = Events.insert({name: list.name, text: list.text, createdAt: new Date(timestamp) });
-
-    });
   }
 });
--- snitch.js
+++ snitch.js
@@ -3,8 +3,15 @@ Events = new Mongo.Collection("events");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    events: function () {
-      return Events.find({});
-    }
   });
 }
+/*
+    events: function (app) {
+      if (app) { 
+        return Events.find({'app':app})
+      } else {
+        return Events.find({});
+      }
+    }
+*/
+
